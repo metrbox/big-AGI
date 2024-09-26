54268c4ac981c35c0923a8ac260bdd695e87f8be
MetrBox <148833600+metrbox@users.noreply.github.com>
2024-09-26 13:21:04 -0400

    Update app.config.ts
    big-AGI

--- src/common/app.config.ts
+++ src/common/app.config.ts
@@ -8,8 +8,8 @@
  */
 export const Brand = {
   Title: {
-    Base: 'big-AGI',
-    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'big-AGI',
+    Base: 'MetrBox',
+    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'MetrBox',
   },
   Meta: {
     Description: 'Unlock the full potential of AI, with precise control over your data and models. Voice interface, AI personas, advanced features, and fun UX.',
