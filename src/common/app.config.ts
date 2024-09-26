38d69095f2a2267a5ef202dd86366c6bf3d29b9d
MetrBox <148833600+metrbox@users.noreply.github.com>
2024-09-26 07:49:57 -0400

    Update app.config.ts

--- src/common/app.config.ts
+++ src/common/app.config.ts
@@ -8,8 +8,8 @@
  */
 export const Brand = {
   Title: {
-    Base: 'MetrBox',
-    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'MetrBox',
+    Base: 'big-AGI',
+    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'big-AGI',
   },
   Meta: {
     Description: 'Unlock the full potential of AI, with precise control over your data and models. Voice interface, AI personas, advanced features, and fun UX.',
